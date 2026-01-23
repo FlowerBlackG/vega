ddec44a03ca3d9eadfaed0bea9a928e093f1c283
GuanTouYu <fb2466375479@outlook.com>
2026-01-23 19:55:58 +0800

    add test: throws.

---
+++ test/throws.cc
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MulanPSL-2.0
+
+#include <cassert>
+#include <chrono>
+
+#include <vega/Scheduler.h>
+#include <vega/Promise.h>
+
+using namespace vega;
+
+static bool suspendMainCompleted = false;
+
+Promise<> throws() {
+    throw std::runtime_error("throws");
+    co_return;  // make this a suspendable function.
+}
+
+
+Promise<> throwInThrow(bool await) {
+    if (await) {
+        co_await throws();
+    } else {
+        throws();
+    }
+}
+
+
+Promise<> suspendMain() {
+    try {
+        throws();
+        co_await Scheduler::get().delay(std::chrono::milliseconds(20));
+    } 
+    catch (...) {
+        assert(false);
+    }
+
+
+    try {
+        co_await throws();
+        assert(false);
+    } 
+    catch (const std::exception& e) {
+        // ok
+    }
+
+    try {
+        throwInThrow(true);
+        throwInThrow(false);
+        co_await throwInThrow(false);
+    } 
+    catch (...) {
+        assert(false);
+    }
+
+
+    try {
+        co_await throwInThrow(true);
+        assert(false);
+    } 
+    catch (const std::exception& e) {
+        // ok
+    }
+
+
+    suspendMainCompleted = true;
+    co_return;
+}
+
+
+int main() {
+    Scheduler::get().runBlocking(suspendMain);
+    assert(suspendMainCompleted);
+    return 0;
+}
